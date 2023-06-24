7a8042a086d049998ac8fb8ac62129760e2ec078
somukhan <mhsomrat154@gmail.com>
2023-06-24 14:43:02 +0600

    Day 10 Completed.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:practice/pages/home_page.dart';
 import 'package:practice/pages/login_page.dart';
 import 'package:practice/utils/my_routes.dart';
---
+++ lib/models/catalog.dart
@@ -0,0 +1,16 @@
+class Item {
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String imageUrl;
+
+  Item(
+      {required this.id,
+      required this.name,
+      required this.desc,
+      required this.price,
+      required this.color,
+      required this.imageUrl});
+}
